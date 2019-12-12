2628b372e077539a941564e0fcb0e4a4563d58f5
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-12 03:44:57 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,27 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  
+  
+  if block_given?
+    counter = 0 
+    while counter < array.length
+      yield (array[counter])
+      counter += 1
+    end
+  array
+
 end
+
+end
+
+
+# else
+#     return "This block should not run!"
+#   end
+
+
+# my_each(['hi', 'hello', 'bye', 'goodbye']) do |word|
+#   p word
+# end
+
+# my_each(['arel', 'jon', 'logan', 'spencer']) do |name|
+#   array
